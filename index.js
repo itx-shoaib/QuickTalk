1aa6eafbe8ec5b037d952e6641008bec565089a3
daniyalayyaz <daniyalayyaz86@gmail.com>
2023-12-05 15:45:26 +0500

    making the socket functions

--- index.js
+++ index.js
@@ -12,12 +12,38 @@ const server = http.createServer(app);
 
 // Configuring CORS options
 const corsOptions = {
-  origin: "*",  // Allow requests from any origin (replace with specific origin in production).
-  methods: ["GET", "POST"]
+    origin: "*",  // Allow requests from any origin (replace with specific origin in production).
+    methods: ["GET", "POST"]
 };
 
 // Initializing Socket.IO with the server and CORS options
 const io = socketIO(server, { cors: corsOptions });
+
+// Handling Socket.IO connections
+io.on("connection", (socket) => {
+
+    // Emitting the user's ID to the connected socket
+    socket.emit("myId", socket.id);
+
+    // Handling socket disconnect event
+    socket.on("disconnect", () => {
+        // Broadcasting a "callEnded" event to all connected sockets
+        socket.broadcast.emit("callEnded");
+    });
+
+    // Handling "callUser" event
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+        // Emitting a "callUser" event to the specified user with relevant data
+        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+
+    // Handling "answerCall" event
+    socket.on("answerCall", (data) => {
+        // Emitting a "callAccepted" event to the specified user with signal data
+        io.to(data.to).emit("callAccepted", data.signal);
+    });
+});
+
 
 // Using CORS middleware for the Express app
 app.use(cors());
@@ -26,11 +52,11 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 // Making the route to the  server
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     return res.send(`Server is running`)
 })
 
 // Starting the server and logging the port
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
